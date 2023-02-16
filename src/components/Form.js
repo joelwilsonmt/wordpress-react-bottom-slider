56c74c263bea2ab137fda9d2d02b904aa2a52799
Cassi <cassi.gallagher-shearer@submittable.com>
2023-02-16 12:52:18 -0800

    Remove chilipiper

--- src/components/Form.js
+++ src/components/Form.js
@@ -27,34 +27,6 @@ export default React.memo((props) => {
 		});
 		let lead = {};
 
-		if (props.cpTenantDomain) {
-			window.addEventListener("message", function (event) {
-				if (event.data.type === "hsFormCallback") {
-					console.log(event.data);
-					if (event.data.eventName === "onFormSubmit") {
-						console.log("onFormSubmit");
-						window.lead = {};
-						for (var key in event.data.data) {
-							if (Array.isArray(event.data.data[key].value)) {
-								event.data.data[key].value = event.data.data[key].value.toString();
-							}
-							lead[event.data.data[key].name] = event.data.data[key].value;
-						}
-						if (Object.keys(lead).length <= 1) {
-							lead = event.data.data;
-						}
-					} else if (event.data.eventName === "onFormSubmitted") {
-						console.log("onFormSubmitted");
-						console.log(lead);
-						ChiliPiper.submit(props.cpTenantDomain, props.cpRouterName, {
-							title: "Thanks! What time works best for a quick call?",
-							map: true,
-							lead: lead,
-						});
-					}
-				}
-			});
-		}
 		// must remove these children else becomes unwieldy:
 		return () => {
 			document.querySelectorAll("[id^=hubspot-embed]").forEach((e) => e.parentNode.removeChild(e));
